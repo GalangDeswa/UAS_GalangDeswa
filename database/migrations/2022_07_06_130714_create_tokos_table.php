8582ddda6188727cd473b464604e85f1e9558c07
GalangDeswa <108006782+GalangDeswa@users.noreply.github.com>
2022-07-06 20:15:26 +0700

    +migration (users + hak_akses)

---
+++ database/migrations/2022_07_06_130714_create_tokos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokos', function (Blueprint $table) {
+            $table->id('id_toko');
+            $table->string('nama_toko');
+            $table->string('desc');
+            $table->bigInteger('id_kat_toko');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokos');
+    }
+};
